8abd085728af532308694903e43f7bf66d798a8a
Ian Brown <icb0004@auburn.edu>
2018-09-19 23:31:50 -0500

    finished ReverseVowels2

--- ReverseVowels2.py
+++ ReverseVowels2.py
@@ -1,9 +1,33 @@
 import sys
 
-word_list = [a,e,i,o,u]
-for i in sys.stdin:
-    a = i.split()
+vowels = ['a','e','i','o','u']
 
+def reverse_vowels(string):
+    string = list(string)
+    left_ptr = 0
+    right_ptr = len(string) - 1 
+    temp = ""
+    while left_ptr < right_ptr:
+        if string[left_ptr].lower() not in vowels:
+            left_ptr += 1
+        elif string[right_ptr].lower() not in vowels:
+            right_ptr -= 1
+        else:
+            temp = string[right_ptr]
+            string[right_ptr] = string[left_ptr]
+            string[left_ptr] = temp 
+            right_ptr -= 1
+            left_ptr += 1
+    return "".join(string)
 
-for char in a: 
-    
+def contains_vowel(word):
+    for char in word:
+        if char in vowels:
+            return True
+        else:
+            continue 
+    return False
+
+if __name__=='__main__':
+    test_string = input("Input a string: ")
+    print(reverse_vowels(test_string))
