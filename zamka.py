9d90da7ef4cfb2043b12cb22a0ea4ae99c695d81
Ian Brown <icb0004@auburn.edu>
2018-09-21 15:05:55 -0500

    added .gitignore and deleted class files

---
+++ zamka.py
@@ -0,0 +1,19 @@
+import sys
+
+def minimal_n(L, D, X):
+    return 0
+
+def maximal_m(L, D, X): 
+    return 0
+
+
+if __name__ == "__main__":
+    nums = sys.stdin.readlines().split
+    L = nums[0]
+    D = nums[1] 
+    X = nums[2] 
+    print(L)
+    print(D)
+    print(X)
+
+
