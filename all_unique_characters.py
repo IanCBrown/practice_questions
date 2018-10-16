755f2ea5dbbeeabf5253f2681eed915fde5f9db9
Ian Brown <icb0004@auburn.edu>
2018-10-16 12:19:54 -0500

    Cracking the coding interview problem 1.1

---
+++ all_unique_characters.py
@@ -0,0 +1,22 @@
+
+# Cracking the Coding Interview Problem 1.1 
+
+def check_unique(string):
+    visited = set()
+    for char in string:
+        visited.add(char)
+    
+    if len(visited) == len(string):
+        return True
+    else:
+        return False 
+
+
+def main(): 
+    str_in = input("Input a string to check for all unique characters:\n")
+
+    print(check_unique(str_in))
+
+
+if __name__ == "__main__":
+    main()
