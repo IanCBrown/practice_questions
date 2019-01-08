a6f24ebb9aa45c44f3ec6bcd4b56ca5debb94c24
Ian Brown <icb0004@auburn.edu>
2019-01-07 23:08:20 -0600

    running sum in python

---
+++ running_sum.py
@@ -0,0 +1,15 @@
+
+def running_sum(l):
+    """ Calculate running sum of list l"""
+    ret_list = [] 
+    ret_list = [sum(l[0:i + 1]) for i in range(len(l))] 
+    return ret_list
+
+def main():
+    test = [10, 20, 30, 40, 50]
+    print(running_sum(test))
+
+
+
+if __name__ == '__main__':
+    main()
