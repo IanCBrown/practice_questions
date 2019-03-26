9a592971f4783888fed2745ac393b5f3caccd09c
Ian Brown <icb0004@auburn.edu>
2019-03-26 11:01:06 -0500

    poorly worded hackerrank problem with confusing results

---
+++ picking_numbers_my_interpretation.py
@@ -0,0 +1,20 @@
+# my interpretation of 
+# https://www.hackerrank.com/challenges/picking-numbers/problem
+
+def pickingNumbers(a):
+    ret = []
+    for num in a:
+        temp = [] 
+        for i in range(len(a)):
+            if abs(num - a[i]) <= 1:
+                temp.append(a[i])
+        print(temp)
+        if abs(max(temp) - min(temp)) <= 1:
+            ret.append(temp)
+
+    l = 0 
+    for arr in ret:
+        if l < len(arr):
+            l = len(arr)
+    print(ret) 
+    return l
