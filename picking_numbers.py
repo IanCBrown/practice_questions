e138a3a19e81e94869f74c1abe7e9dcfd31fbd5d
Ian Brown <icb0004@auburn.edu>
2019-03-26 23:49:07 -0500

    picking numbers using frequency array

---
+++ picking_numbers.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# https://www.hackerrank.com/challenges/picking-numbers/problem
+def pickingNumbers(a):
+    num = 0 
+    freq = [0] * 100
+
+    for i in range(len(a)):
+        freq[a[i]] += 1    
+    
+    for i in range(1, 100):
+        num = max(num, freq[i] + freq[i - 1])
+    return num 
