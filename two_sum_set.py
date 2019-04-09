48f889dd0e04461ae8ddc40060e4883c7d6be54e
Ian Brown <icb0004@auburn.edu>
2019-04-09 12:54:32 -0500

    updated two_sum_set solution

--- two_sum_set.py
+++ two_sum_set.py
@@ -9,7 +9,18 @@ def two_sum_set(arr, target):
     return False
 
 
+def two_sum_set_ret(arr, target):
+    saved = set()
+    for i in range(len(arr)):
+        if (target - arr[i]) in saved:
+            return (arr[i], target - arr[i])
+        else:
+            saved.add(arr[i])
+    return False
+
+
 if __name__ == "__main__":
     test_arr = [2,4,5,9,1]
     target = int(input())
     print(two_sum_set(test_arr, target))
+    print(two_sum_set_ret(test_arr, target))
