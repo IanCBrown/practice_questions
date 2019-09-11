cf2020afa26342ee744fd949c3fe285c71e4c033
Ian Brown <icb0004@auburn.edu>
2019-09-11 16:57:33 -0500

    CTCI compression

--- compression.py
+++ compression.py
@@ -7,27 +7,41 @@ def compress(string):
     for char in string:
         if char not in counts:
             counts[char] = list(string).count(char)
-
+            
     ret = []
-    for key, value in counts:
-        ret.append(key + value)
+    for key, value in counts.items():
+        ret.append(key + str(value))
 
-    print(ret)
+    return "".join(ret)
 
 def compress_slow(string):
     # slow but correct O(n^2) solution 
     ret = [] 
     i = 0
-    while i < (len(string) - 1):
+    while i < (len(string)):
         j = i + 1
         count = 1
-        while string[i] == string[j] and j < len(string) - 1:
-           count += 1
-           j += 1         
+        while j < len(string) and string[i] == string[j]:
+            count += 1
+            j += 1       
         ret.append(string[i] + str(count))
         i = j 
     return "".join(ret)
 
 
-# print(compress("aabcccccaaa"))
+def compress_better(string):
+    ret = [] 
+    count_consecutive = 0 
+
+    for i in range(len(string)):
+        count_consecutive += 1
+
+        if (i + 1 >= len(string) or string[i] != string[i + 1]):
+            # ret.append(string[i].join([str(count_consecutive)]))
+            ret.append(string[i] + str(count_consecutive))
+            count_consecutive = 0 
+    return "".join(ret)
+
+print(compress("aabcccccaaa"))
 print(compress_slow("aabcccccaaa"))
+print(compress_better("aabcccccaaa"))
