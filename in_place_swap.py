95a456f1d0e0af24374f3a663a4b93aebd241a1f
Ian Brown <icb0004@auburn.edu>
2020-01-25 01:00:15 -0600

    test

---
+++ in_place_swap.py
@@ -0,0 +1,18 @@
+
+
+def swap(a,b):
+    # ex
+    # a = 5
+    # b = 7
+    a = a * b # a = 35
+
+    b = a / b # b = 5
+    a = a / b # a = 7 
+
+    print(a)
+    print(b)
+
+# driver
+a = 5
+b = 7
+swap(a,b)
