dcbd72c3eee3644d62c7c1c546a6ffbaaa3ee867
Ian Brown <icb0004@auburn.edu>
2020-03-23 19:04:53 -0500

    relearning dp on leetcode

---
+++ min_stair_climb.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        if cost is None:
+            return 0
+        dp = [0]*len(cost)
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+        
+        for i in range(2, len(cost)):
+            dp[i] = cost[i] + min(dp[i-1],dp[i-2])
+        
+
+        return min(dp[-1], dp[len(cost) - 2])
